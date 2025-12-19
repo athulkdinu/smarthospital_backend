8718af9c390ee3c4556134ce8c4773985c581f2f
athulkdinu <athulkdinu@gmail.com>
2025-12-19 21:20:42 +0530

    final commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const router=JSONServer.router("db.json")
 
 //4.defime port to run the server
 
-const PORT=3000 || process.env.PORT
+const PORT=process.env.PORT || 3000
 //5.use middleware
 
 smarthospital.use(middleware) 
